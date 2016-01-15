c16bbba11d89950483890846c45619356d706c6f
cntorres1691 <clao91torrestorres@gmal.com>
2016-01-15 13:58:45 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,32 +1,15 @@
 <?php
-session_start();
+ 	session_start();
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<title> PSW + Cookie pagina + nombre  </title>
-	<link type="txt/css" rel="stylesheet" href="css/main.css"/>
-</head>
-<body>
-<header>
-</header>
-<section id="sidebar">
-</section>
-<section id="main">
-
-<?php 
-if(isset($_SESSION['nombre'])){
+<?php
+ if (isset($_SESSION['nombre'])){
 	session_destroy();
-	echo "se ha destruido sesion exitosamente <br/>";
+	echo "se ha destrudi sesion exitosamente <br/>";
 	echo "<a href='index.php'>Volver</a>";
 }else{
-	echo "Error.... <br/>";
+	echo "Error....<br/>";
 	echo "<a href='index.php'>Volver</a>";
-
+}
 
 ?>
-</section>
-</body>
-</html>
+
